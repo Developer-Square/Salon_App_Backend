61c39cc2178ccb959f5a24efcc22b4362e6345bd
MutwiriFrank <mutwirifranco@gmail.com>
2021-07-21 10:08:33 +0300

    ädd first_name

--- social_auth/google.py
+++ social_auth/google.py
@@ -1,6 +1,8 @@
 from google.auth.transport import requests
 from google.oauth2 import id_token
 
+CLIENT_ID = '555068933656-haou46l4vec87gf7akedbudgm653c1a6.apps.googleusercontent.com'
+
 
 class Google:
     """Google class to fetch the user info and return it"""
@@ -11,10 +13,9 @@ class Google:
         validate method Queries the Google oAUTH2 api to fetch the user info
         """
         try:
-            idinfo = id_token.verify_oauth2_token(
-                auth_token, requests.Request())
+            idinfo = id_token.verify_oauth2_token(auth_token, requests.Request())
 
-            if 'accounts.google.com' in idinfo['iss']:
+            if 'accounts.google.com' in idinfo['iss']:  #issue it matches if string is in  'accouunts.google.com' or 'https://accouunts.google.com'
                 return idinfo
 
         except:
--- social_auth/serializers.py
+++ social_auth/serializers.py
@@ -5,19 +5,21 @@ import os
 from rest_framework.exceptions import AuthenticationFailed
 
 
+CLIENT_ID = '555068933656-haou46l4vec87gf7akedbudgm653c1a6.apps.googleusercontent.com'
+
 class GoogleSocialAuthSerializer(serializers.Serializer):
     auth_token = serializers.CharField()
 
     def validate_auth_token(self, auth_token):
         user_data = google.Google.validate(auth_token)
         try:
-            user_data['sub']
+            user_data['sub'] #'sub' user id to store user info
         except:
             raise serializers.ValidationError(
                 'The token is invalid or expired. Please login again.'
             )
 
-        if user_data['aud'] != '555068933656-haou46l4vec87gf7akedbudgm653c1a6.apps.googleusercontent.com':
+        if user_data['aud'] != CLIENT_ID:  # google client id
 
             raise AuthenticationFailed('oops, who are you?')
 
